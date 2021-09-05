a0933a3343293c711c14fa67d32cad3914e85447
Joel Gonzalez <joelgonzalezmedina1992@gmail.com>
2021-09-04 21:06:32 -0400

    clear project

--- src/App.js
+++ src/App.js
@@ -1,16 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React, { useState } from 'react'
+import { isEmpty } from 'lodash'
+import shortid from 'shortid'
 
 function App() {
+  const [task, setTask] = useState("")
+  const [tasks, setTasks] = useState([])
+
+  const addTask = (e) => {
+    e.preventeDefault()
+    if (isEmpty(task)) {
+      console.log("Task Empty")
+      return     
+    } 
+    const newTask={
+      id: shortid.generate(), 
+      name: tasks
+    }
+   
+    setTasks([...tasks, newTask])
+    setTask("")
+  }
+
+
   return (
 <div className="container mt-5"> 
-
-  <h1>Hola mundo klk!</h1>
+  <h1 className ="text-center">Sistema de Gestión de Tareas</h1>
   <hr></hr>
-   
-   
+    <div className = "row">
+          <div className = "col-8"> 
+          <h4 className="text-center">Lista de Tareas</h4>
+          <ul className ="list-group">
+
+        {
+          tasks.map((task)=>(
+            <li className ="list-group-item" key={task.id}>
+             <span className="lead">{task.name}</span>
+             <button className ="btn btn-danger btn-sm float-right mx-2"> Eliminar </button>
+             <button className ="btn btn-warning btn-sm float-right" > Editar </button>        
+         </li>
+          ))
+        
+        
+          }
+
+          </ul>
+          </div>
+         <div className ="col-4"> 
+          <h4 className ="text-center">Formulario</h4>
+          <form onSubmit={addTask}>
+          <input type ="text"
+          className ="form-control  mb-2"
+          placeholder ="Ingrese la tarea............."
+          onChange={(text)=> setTask(text.target.value)}
+          value={task}
+          />          
+          <button className="btn btn-dark btn-block" 
+          type="submit">
+            Agregar
+            </button>
+
+          </form>
+
+        </div>       
+    </div>    
 </div>
   );
 }
 
-export default App;
+export default App; 
